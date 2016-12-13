24dbfe5e02aba462b5575653d5e2bf32d978a6e0
Andrey <ansiz7777@gmail.com>
2016-12-13 22:53:53 +0300

    more_points

--- HSEBuildings.UI/Map.cs
+++ HSEBuildings.UI/Map.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -34,16 +37,27 @@ namespace HSEBuildings.UI
             gMapControl1.Zoom = 12;
             gMapControl1.Dock = DockStyle.Fill;
             gMapControl1.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            
+            GMapOverlay markersOverlay = new GMapOverlay("marker");
+            markersOverlay.Markers.Add(CreateNewMarker(new PointLatLng(55.778651, 37.733001), "ул. Кирпичная, 33", "Кирпичная"));
+            markersOverlay.Markers.Add(CreateNewMarker(new PointLatLng(55.761389, 37.633100), "ул. Мясницкая, 20", "Мясницкая20"));
+            markersOverlay.Markers.Add(CreateNewMarker(new PointLatLng(55.761389, 37.632200), "ул. Мясницкая, 11", "Мясницкая11"));
+            markersOverlay.Markers.Add(CreateNewMarker(new PointLatLng(55.720181, 37.608234), "ул. Шаболовка, 26", "Мясницкая26"));
+            markersOverlay.Markers.Add(CreateNewMarker(new PointLatLng(55.803296, 37.409504), "ул. Таллинская, 34", "МИЭМ"));
+            
 
-            GMap.NET.WindowsForms.GMapOverlay markersOverlay = new GMap.NET.WindowsForms.GMapOverlay("marker");
-            GMap.NET.WindowsForms.Markers.GMarkerGoogle markerG = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new GMap.NET.PointLatLng(55.778651, 37.733001), GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red);
-            markerG.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(markerG);
-            markerG.ToolTipText = "ул. Кирпичная, 33";
-            markerG.Tag = "Кирпич";
-            markersOverlay.Markers.Add(markerG);
+
             gMapControl1.Overlays.Add(markersOverlay);
-            gMapControl1.Position = new GMap.NET.PointLatLng(55.75393, 37.620795);
+            gMapControl1.Position = new PointLatLng(55.75393, 37.620795);
+        }
+        public GMapMarker CreateNewMarker(PointLatLng coordinates,string text,string tag)
+        {
+            GMarkerGoogle marker = new GMarkerGoogle(coordinates, GMarkerGoogleType.red);
+            marker.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(marker);
+            marker.ToolTipText = text;
+            marker.Tag = tag;
+            return marker;
         }
     }
 }
