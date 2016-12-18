aaa829c70f20bb3c76c973be5609073c5fe505a1
dspetrov <Daimon@DESKTOP-8ROT722>
2016-12-18 22:27:40 +0300

    Some json file fixes

--- HSEBuildings.Data/Migrations/Configuration.cs
+++ HSEBuildings.Data/Migrations/Configuration.cs
@@ -4,6 +4,9 @@ namespace HSEBuildings.Data.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Reflection;
+    using Newtonsoft.Json;
+    using System.IO;
 
     internal sealed class Configuration : DbMigrationsConfiguration<HSEBuildings.Data.Context>
     {
@@ -14,6 +17,17 @@ namespace HSEBuildings.Data.Migrations
 
         protected override void Seed(HSEBuildings.Data.Context context)
         {
+            //var resourceName = "Builings.Json";
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var stream = assembly.GetManifestResourceStream(resourceName);
+            //var serializer = new JsonSerializer();
+
+            //using (var sr = new StreamReader(stream))
+            //using (var jsonTextReader = new JsonTextReader(sr))
+            //{
+            //    var jsones= serializer.Deserialize(jsonTextReader);
+            //}
+            
             Repository repo = new Repository();
             repo.GetData();
             foreach (var item in repo.Campuses)
@@ -26,7 +40,7 @@ namespace HSEBuildings.Data.Migrations
             }
             foreach (var item in repo.Sides)
             {
-                context.Side.AddOrUpdate(side=> side.Name, item);
+                context.Side.AddOrUpdate(side => side.Name, item);
             }
             foreach (var item in repo.SideFlors)
             {
